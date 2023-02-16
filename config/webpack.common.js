aed963192e3401c6d0b79a76b551b69d03ce874d
peishuangheng <peishuangheng@hualala.com>
2023-02-16 17:25:51 +0800

    faat: webpack 配置优化

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,14 +1,13 @@
 const path = require("path");
 const WebpackBar = require("webpackbar");
-const Jarvis = require("webpack-jarvis");
-var DashboardPlugin = require("webpack-dashboard/plugin");
 let progressPlugin = new WebpackBar({
   color: "#85d", // 默认green，进度条颜色支持HEX
-  basic: false, // 默认true，启用一个简单的日志报告器
+  basic: true, // 默认true，启用一个简单的日志报告器
   profile: false, // 默认false，启用探查器。
 });
 module.exports = {
   entry: "./src/index.js",
+  mode: "development",
   output: {
     filename: "[contenthash].main.js",
     path: path.resolve(__dirname, "../dist"),
